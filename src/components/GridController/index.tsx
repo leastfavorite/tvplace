42de29d1b69705108ee5b4b6e670cefaa0def69e
leastfavorite <ariaschnei@gmail.com>
2025-09-15 13:48:26 -0500

    pan and zoom controls

--- src/components/GridController/index.tsx
+++ src/components/GridController/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { MouseEvent as ReactMouseEvent, useEffect, useRef } from "react"
+import { PointerEvent as ReactPointerEvent, useEffect, useRef } from "react"
 import { useController, UseControllerProps } from "react-hook-form";
 import { FormValues } from "../Form";
 import Grid from "../Grid";
@@ -8,111 +8,186 @@ import Grid from "../Grid";
 import styles from "./style.module.css";
 import settings from "../../place.config.json";
 
+interface PointerData {
+  id: number,
+  ix: number,
+  iy: number,
+  cx: number,
+  cy: number
+}
+
+interface CameraData {
+  pointers: PointerData[],
+  zooming: boolean,
+  x: number,
+  y: number,
+  scale: number
+}
+
 export default function GridController({ ...props }: UseControllerProps<FormValues>) {
 
   const { field, fieldState } = useController(props);
-  const divRef = useRef<HTMLDivElement>(null);
 
-  const camRef = useRef({
-    downX: 0,
-    downY: 0,
-    currentX: 0,
-    currentY: 0,
-    mouseDown: false,
+  const zoomRef = useRef<HTMLDivElement>(null);
+  const zoomContainerRef = useRef<HTMLDivElement>(null);
+  const panRef = useRef<HTMLDivElement>(null);
+  const cursorRef = useRef<HTMLDivElement>(null);
 
+  const cameraRef = useRef<CameraData>({
+    pointers: [],
+    zooming: false,
     x: 0,
     y: 0,
-    scale: 8
+    scale: 5
   })
 
-  const isDrag = () => {
-    const cam = camRef.current;
-    const dx = (cam.currentX - cam.downX)
-    const dy = (cam.currentY - cam.downY)
-
-    return (dx * dx + dy * dy) > (cam.scale * cam.scale)
+  const pointerDown = (p: ReactPointerEvent) => {
+    const cam = cameraRef.current;
+    if (cam.pointers.length < 2) {
+      cam.pointers.push({
+        id: p.pointerId,
+        ix: p.clientX,
+        iy: p.clientY,
+        cx: p.clientX,
+        cy: p.clientY
+      })
+      if (cam.pointers.length === 2) {
+        cam.zooming = true;
+      }
+    }
   }
 
-  const updateCamera = () => {
-    const div = divRef.current;
-    const cam = camRef.current;
-    if (div) {
-      let x = cam.x;
-      let y = cam.y;
-      if (cam.mouseDown && isDrag()) {
-        x += (cam.currentX - cam.downX) / cam.scale;
-        y += (cam.currentY - cam.downY) / cam.scale;
-      }
-      div.style.transform = `scale(${cam.scale}) translate(${x}px, ${y}px)`
+  const pointerMove = (p: PointerEvent) => {
+    const cam = cameraRef.current;
+
+    const evt = cam.pointers.find(p2 => p.pointerId === p2.id)
+    if (evt) {
+      evt.cx = p.clientX;
+      evt.cy = p.clientY;
+
+      render();
     }
+  }
+
+  const pointerUp = (p: PointerEvent) => {
+    const cam = cameraRef.current;
+
+    const idx = cam.pointers.findIndex((p2) => p.pointerId === p2.id)
+    if (idx === -1) {
+      return
+    }
+
+    if (cam.pointers.length > 0) {
+      render(true) || applyClick();
+    }
+
+    cam.pointers.splice(idx, 1);
+    if (cam.pointers.length === 0) {
+      cam.zooming = false;
+    }
+
+  }
+
+  const applyClick = () => {
+  }
+
+  // apply: applies the current gesture to the camera
+  // returns *true* if the
+  const render = (apply: boolean = false): boolean => {
+    const cam = cameraRef.current;
+
+    let x = cam.x;
+    let y = cam.y;
+
+    let scale = cam.scale;
+
+    if (cam.pointers.length === 2) {
+      const p1 = cam.pointers[0]
+      const p2 = cam.pointers[1]
+
+      const ix = (p1.ix + p2.ix) / 2;
+      const iy = (p1.iy + p2.iy) / 2;
+      const cx = (p1.cx + p2.cx) / 2;
+      const cy = (p1.cy + p2.cy) / 2;
+
+      const dcx = p2.cx - p1.cx;
+      const dcy = p2.cy - p1.cy
+      const dix = p2.ix - p1.ix
+      const diy = p2.iy - p1.iy
+
+      const dc = dcx * dcx + dcy * dcy;
+      const di = dix * dix + diy * diy;
+
+      const scaleFactor = Math.sqrt(dc / di);
+      scale *= scaleFactor;
+
+      x = cx - scaleFactor * (ix - cam.x)
+      y = cy - scaleFactor * (iy - cam.y)
+
+    } else if (cam.pointers.length === 1) {
+      const p = cam.pointers[0]
+
+      const dx = p.cx - p.ix;
+      const dy = p.cy - p.iy;
+
+      if (!cam.zooming && (dx * dx + dy * dy < cam.scale * cam.scale)) {
+        return false;
+      }
+
+      x += dx;
+      y += dy;
+    }
+
+    if (apply) {
+      cam.x = x;
+      cam.y = y;
+      cam.scale = scale;
+
+      cam.pointers.forEach((p) => {
+        p.ix = p.cx;
+        p.iy = p.cy;
+      })
+    }
+
+    const pan = panRef.current;
+    if (pan) {
+      pan.style.transform = `translate(${x}px, ${y}px)`
+    }
+
+    const zoom = zoomRef.current;
+    const zoomContainer = zoomContainerRef.current;
+    if (zoom && zoomContainer) {
+      zoom.style.transform = `scale(${scale})`
+      zoomContainer.style.width = `${scale * settings.width}px`;
+      zoomContainer.style.height = `${scale * settings.height}px`;
+    }
+
+    return true;
   }
 
   useEffect(() => {
-    updateCamera();
-  }, [divRef]);
-
-  const mouseDown = (m: ReactMouseEvent<HTMLDivElement>) => {
-    if (m.buttons & 1) {
-      const cam = camRef.current;
-      cam.downX = m.clientX;
-      cam.downY = m.clientY;
-      cam.mouseDown = true;
-    }
-  }
-
-  const mouseUp = (m: MouseEvent) => {
-    const cam = camRef.current;
-    if (cam.mouseDown) {
-      if (isDrag()) {
-        cam.x += (m.clientX - cam.downX) / cam.scale;
-        cam.y += (m.clientY - cam.downY) / cam.scale;
-      } else {
-        console.log("click!");
-
-        const div = divRef.current;
-        if (div) {
-          const r = div.getBoundingClientRect();
-          const x = Math.floor((cam.downX - r.x) / cam.scale);
-          const y = Math.floor((cam.downY - r.y) / cam.scale);
-          if (x >= 0 && x < settings.width && y >= 0 && y < settings.height) {
-            field.onChange(y * settings.width + x);
-          }
-        }
-      }
-    }
-    cam.mouseDown = false;
-    updateCamera();
-  }
-
-  const mouseMove = (m: MouseEvent) => {
-    const cam = camRef.current;
-    cam.currentX = m.clientX;
-    cam.currentY = m.clientY;
-    updateCamera();
-  }
-
-  useEffect(() => {
-    window.addEventListener("mouseup", mouseUp);
-    window.addEventListener("mouseleave", mouseUp);
-    window.addEventListener("mousemove", mouseMove);
+    window.addEventListener("pointerup", pointerUp);
+    window.addEventListener("pointerleave", pointerUp);
+    window.addEventListener("pointermove", pointerMove);
+    render();
 
     return () => {
-      window.removeEventListener("mouseup", mouseUp);
-      window.removeEventListener("mouseleave", mouseUp);
-      window.removeEventListener("mousemove", mouseMove);
+      window.removeEventListener("pointerup", pointerUp);
+      window.removeEventListener("pointerleave", pointerUp);
+      window.removeEventListener("pointermove", pointerMove);
     }
   }, []);
 
-
   return (
-    <div className={styles.container}>
-      <div
-        className={styles.camera}
-        ref={divRef}
-        onMouseDown={mouseDown}
-      >
-        <Grid colors={settings.colors} width={settings.width} height={settings.height} />
+      <div className={styles.pan} ref={panRef}>
+
+        <div className={styles.zoomContainer} ref={zoomContainerRef}>
+          <div className={styles.zoom} ref={zoomRef} onPointerDown={pointerDown}>
+            <Grid colors={settings.colors} width={settings.width} height={settings.height} />
+          </div>
+        </div>
+
+        <div className={styles.cursor} ref={cursorRef} />
       </div>
-    </div>
   )
 }
